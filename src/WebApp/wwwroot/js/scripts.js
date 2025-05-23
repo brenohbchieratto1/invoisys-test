bf390bc6ae74095e04774f8766890b8fc78cf453
Chieratto <brenohbchieratto@gmail.com>
2025-05-22 22:36:21 -0300

    start frontend development

---
+++ src/WebApp/wwwroot/js/scripts.js
@@ -0,0 +1,8 @@
+﻿window.customJs = {
+    showAlert: function (message) {
+        alert("Mensagem via jQuery: " + message);
+    },
+    animateCounter: function () {
+        $("#counterText").fadeOut(200).fadeIn(200);
+    }
+};
